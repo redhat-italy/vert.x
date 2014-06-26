68b5fcd76687bd460236989a14b02080c57ff002
mrizzi <mrizzi@redhat.com>
2014-06-26 18:34:51 +0200

    set GlobalConfigurationBuilder classLoader

--- vertx-infinispan/src/main/java/org/vertx/java/spi/cluster/impl/infinispan/InfinispanClusterManager.java
+++ vertx-infinispan/src/main/java/org/vertx/java/spi/cluster/impl/infinispan/InfinispanClusterManager.java
@@ -90,6 +90,7 @@ public class InfinispanClusterManager implements ClusterManager {
         }
         GlobalConfiguration globalConfiguration = new GlobalConfigurationBuilder()
                 .clusteredDefault()
+                .classLoader(GlobalConfiguration.class.getClassLoader())
                 .transport().addProperty("configurationFile", "jgroups-udp.xml")
                 .globalJmxStatistics().allowDuplicateDomains(true).enable()
                 .build();
@@ -103,6 +104,7 @@ public class InfinispanClusterManager implements ClusterManager {
     @Override
     public synchronized void leave() {
         System.out.println("LEAVE [" + active + "]");
+        Thread.dumpStack();
         if (!active) {
             return;
         }
